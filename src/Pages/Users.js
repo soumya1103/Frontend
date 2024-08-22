21d5be0b7a3842bb4b92ba7229e1e0d003240559
soumya1103 <soumya.agrawal@nucleusteq.com>
2024-08-22 17:51:18 +0530

    Users UI Completed

--- src/Pages/Users.js
+++ src/Pages/Users.js
@@ -1,14 +1,97 @@
 import React from "react";
 import DashboardHoc from "../Coponents/HOC/DashboardHoc";
+import Button from "../Coponents/Button";
+import Table from "../Coponents/Table";
+import SearchBar from "../Coponents/SearchBar";
+import Operation from "../Coponents/Operation";
 
-function Users(props) {
+function Users() {
+  const columns = [
+    { header: "ID", accessor: "id" },
+    { header: "Name", accessor: "name" },
+    { header: "Email", accessor: "email" },
+    { header: "Operation", accessor: "operation" },
+  ];
+
+  const data = [
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john@example.com",
+      operation: <Operation />,
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      email: "jane@example.com",
+      operation: <Operation />,
+    },
+    {
+      id: 3,
+      name: "Michael Johnson",
+      email: "michael@example.com",
+      operation: <Operation />,
+    },
+    {
+      id: 4,
+      name: "Michael Johnson",
+      email: "michael@example.com",
+      operation: <Operation />,
+    },
+    {
+      id: 5,
+      name: "Michael Johnson",
+      email: "michael@example.com",
+      operation: <Operation />,
+    },
+    {
+      id: 6,
+      name: "Michael Johnson",
+      email: "michael@example.com",
+      operation: <Operation />,
+    },
+    {
+      id: 7,
+      name: "Michael Johnson",
+      email: "michael@example.com",
+      operation: <Operation />,
+    },
+    {
+      id: 8,
+      name: "Michael Johnson",
+      email: "michael@example.com",
+      operation: <Operation />,
+    },
+    {
+      id: 9,
+      name: "Michael Johnson",
+      email: "michael@example.com",
+      operation: <Operation />,
+    },
+    {
+      id: 10,
+      name: "Michael Johnson",
+      email: "michael@example.com",
+      operation: <Operation />,
+    },
+  ];
+
+  const handleSearch = (query) => {
+    // Perform your search logic here, e.g., filter a list or make an API call
+    console.log("Searching for:", query);
+    // Update the searchResults state with the results
+    // setSearchResults(results);
+  };
+
   return (
-    <div>
-      <h1>U</h1>
-      <h1>U</h1>
-      <h1>U</h1>
-      <h1>U</h1>
-      <h1>U</h1>
+    <div className="pages-outer-container">
+      <div className="pages-inner-container">
+        <SearchBar placeholder="Search User" onSearch={handleSearch} />
+        <Button>Add User</Button>
+      </div>
+      <div className="pages-table">
+        <Table columns={columns} data={data} />
+      </div>
     </div>
   );
 }
